298bfc3c65b7c0663af977ec966594ed46862d03
mahdi akhbar <mahdi.akhbar32@gmail.com>
2021-03-12 17:29:59 +0330

    factorial code added

--- app/main.cpp
+++ app/main.cpp
@@ -3,10 +3,22 @@
 #include <stdlib.h>
 
 #include "Foo.h"
+
+int factorial(int n)
+{
+  unsigned int sum{1};
+  for (int i = 1; i <= n; i++) {
+    sum *= i;
+  }
+  return sum;
+}
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
-
+  std::cout << 5 << "! = " << factorial(5) << '\n';
+  std::cout << 0 << "! = " << factorial(0) << '\n';
+  std::cout << 9 << "! = " << factorial(9) << '\n';
+  std::cout << 3 << "! = " << factorial(3) << '\n';
+  
   return 0;
 }
